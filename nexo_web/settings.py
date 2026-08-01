c92f4efa8657c147c5042ddf8490503a0e056bb3
kaleab <kalabadmase9@gmail.com>
2026-08-01 04:05:40 +0300

    Configure STATICFILES_DIRS with absolute BASE_DIR path

--- nexo_web/settings.py
+++ nexo_web/settings.py
@@ -156,9 +156,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',
+# ]
 
 # Modern WhiteNoise configuration (Django 4.2+)
 STORAGES = {
